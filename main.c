697b270021b2e0d2b809fc4bda4ed431de83eef4
baris <bariserayn@gmail.com>
2019-12-22 06:29:22 +0300

    Update main.c
    exit'teki hata düzeltildi

--- main.c
+++ main.c
@@ -32,82 +32,119 @@ int processLine(char* args[], char line[]){
 
 
 int readParseLine(char* args[], char line[]){
-	printf(">");
+
+    printf(">");
     readLine(line);
     processLine(args,line);
+
+    if (!strcmp(args[0],"exit")){
+        exit(1);                       // ??? maindeki while döngüsü içinde stack gibi biriktiriyor. 0layınca exit atıyor???
+    }
+ 
     return 1;
 }
+
+void printHelp(){
+    puts("***********List of Commands supported***************\n"
+        "|\t#convert_to_binary arg1(type) arg2(number) |\n"
+        "|\t#copy_to_desktop arg1(file name)\t   |\n"
+        "|\t#C function-1\t\t\t\t   |\n"
+        "|\t#C function-2\t\t\t\t   |\n"
+        "|\t#exit\t\t\t\t\t   |\n"
+        "|\t#all other commands in UNIX shell\t   |"); 
+    puts("****************************************************");
+}
+
+void welcomeScreen(){
+    char* username = getenv("USER"); 
+    printf("@%s\n", username); 
+    puts("*******************************************");
+    puts("|                                         |");
+    puts("|                                         |");
+    puts("|        EY Shell is starting...          |");
+    puts("|   Type help for supporting commands     |");
+    puts("|                                         |");
+    puts("|                                         |");
+    puts("*******************************************");
+}
+
 void sortTheFile(FILE *file,int size,char b[]){
-		file = fopen(b, "r");
-	    	int a[size];
-	    	int i;
-		
-	    	for (i = 0; i <sizeof(a)/sizeof(a[0]) ; i++)
-	    	{
-	     	   fscanf(file, "%d", &a[i]);
-	   	 }
-		
-		for (int l=0;l<sizeof(a)/sizeof(a[0]);l++){
-		printf("%d\n",a[l]);
-		}
-	    	
-	    	qsort(a,sizeof(a)/sizeof(a[0]),sizeof(int),cmpfunc);
-		printf("\n");
-	     	for (int l=0;l<sizeof(a)/sizeof(a[0]);l++){
-		printf("%d\n",a[l]);		
-		}
-		fclose(file);
-}
- int findTheLinesNumber(FILE*file2,char c[]){
-		file2 = fopen(c, "r");
-
-		int count=0;	   	
-		int num;
-		while(fscanf(file2,"%d",&num)>0){
-		count=count+1;
-		}
-		fclose(file2);
-		return count;
-		fclose(file2);
-
-
-
-
+        file = fopen(b, "r");
+            int a[size];
+            int i;
+        
+            for (i = 0; i <sizeof(a)/sizeof(a[0]) ; i++)
+            {
+               fscanf(file, "%d", &a[i]);
+         }
+        
+        for (int l=0;l<sizeof(a)/sizeof(a[0]);l++){
+        printf("%d\n",a[l]);
+        }
+            
+            qsort(a,sizeof(a)/sizeof(a[0]),sizeof(int),cmpfunc);
+        printf("\n");
+            for (int l=0;l<sizeof(a)/sizeof(a[0]);l++){
+        printf("%d\n",a[l]);        
+        }
+        fclose(file);
 }
 
+int findTheLinesNumber(FILE*file2,char c[]){
+        file2 = fopen(c, "r");
+
+        int count=0;        
+        int num;
+        while(fscanf(file2,"%d",&num)>0){
+        count=count+1;
+        }
+        fclose(file2);
+        return count;
+        fclose(file2);
+}
+   
 
 int main(){
+    int temp = 0;
     welcomeScreen();
     char* args[10];
     char line[100];
 
     while(readParseLine(args,line)){
 
-    	char* functions [] = {"help","convert_to_binary","copy_to_desktop"};
-        char *command = malloc(1000*sizeof(char));
-
+        char* functions [] = {"help","convert_to_binary","copy_to_desktop","c_function_1","c_function_2"};
+        char* command = malloc(1000*sizeof(char));
+        
         pid_t childPid = fork();
         if(childPid == 0){
-	       if (!strcmp(*args,functions[0])){
-                   printHelp();
-	        }
-	 	else if (!strcmp(args[0],functions[1])){
-                    sprintf(command,"%s %s %s","sh ShellFunction1.sh",args[1],args[2]);
-                    system(command);
-	        }
-                else if (!strcmp(args[0],functions[2])){
-                    sprintf(command,"%s %s","sh ShellFunction2.sh",args[1]);
-                    system(command);
-                }
-		else {
-	        	execvp(args[0],args);
-	        }
+            if (!strcmp(*args,functions[0])){
+                printHelp();
+            }
+            else if (!strcmp(args[0],functions[1])){
+                sprintf(command,"%s %s %s","sh ShellFunction1.sh",args[1],args[2]);
+                system(command);
+            }
+            else if (!strcmp(args[0],functions[2])){
+                sprintf(command,"%s %s","sh ShellFunction2.sh",args[1]);
+                system(command);
+            }
+            else if (!strcmp(args[0],functions[3])){
+                puts("c function-1");
+            }
+            else if (!strcmp(args[0],functions[4])){
+                puts("c function-2");
+            }
+
+            else {
+                execvp(args[0],args);
+            }
         }
         else {
             waitpid(childPid, 0,0);
         }
 
+
     }
-    return 0;
+    return 1;
 }
 
